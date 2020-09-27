bfdc024dcf762be3b75b615b474b90159a3e1549
arthurpreis <arpereis@gmail.com>
2020-09-27 20:52:38 -0300

    Plotter initial implementation and fixes

--- main.py
+++ main.py
@@ -3,32 +3,4 @@
 # Arthur Reis 2020
 from sympy.physics.mechanics import ReferenceFrame, Point
 from orbit import Orbit
-import numpy as np
-# from matplotlib.pyplot import plot, show
 
-# Create solar system frame of reference
-
-# Solar = ReferenceFrame('Solar')
-
-# Create solar system
-solar = ReferenceFrame('Solar')
-Sun = Point('Sun')
-Sun.set_vel(solar, 0)
-
-earth_orbit = Orbit(solar, Sun, 0, 1, 0.1, 0, 0)
-earth_pos = earth_orbit.get_position(0)
-Earth = Point('Earth')
-Earth.set_pos(Sun, earth_pos)
-
-earth_frame = solar.orientnew('Earth', 'Body', (0.2, 0, 0), 123)
-sat_orbit = Orbit(earth_frame, Earth, 0.2, 0.3, 0.03, 0, 0)
-# Earth = Point()
-
-# EarthRef = ReferenceFrame() #need to pass epoch!
-
-# create 3 satellites
-
-# orbit stuff
-# define earth orbit
-
-# define satellite orbit?
--- orbit.py
+++ orbit.py
@@ -1,5 +1,4 @@
 import numpy as np
-from mayavi.mlab import *
 from mayavi import mlab
 from sympy.physics.mechanics import *
 from numpy import cos, sin, linspace
@@ -7,6 +6,11 @@ from plotter import Plotter
 
 
 class Orbit:
+    '''Base orbit class that is used to build circular and eliptical orbits
+    frame: sympy.ReferenceFrame object
+    center: point/trajectory of the attractor #todo rename?
+    color: (R,G,B) tuple to be used when plotting trajectory and body'''
+
     def __init__(self, frame, center, color = (0, 0, 0)):
         self.frame = frame
         self.center = center
@@ -16,24 +20,32 @@ class Orbit:
         pass
 
     def get_trajectory(self, epoch: np.array, center = None, frame = None):
+        '''Returns a 2D or 3D numpy array of results from get_position()
+        over time'''
         if type(self) == CircularOrbit3D:
             pos = np.zeros_like([epoch, epoch, epoch], dtype = float)
         elif type(self) == CircularOrbit2D:
             pos = np.zeros_like([epoch, epoch], dtype = float)
 
         if frame is None:
+            # todo: better explanation of how sympy frames work here,
+            # some way to add the possib. of plotting around earth
             frame = self.frame
 
         for idx, ti in enumerate(t):
+            # calls get_position() for each time, subtract center position
             if center is None:
                 vec = self.get_position(ti).to_matrix(frame)
             else:
-                vec = (self.get_position(ti) - center.get_position(ti)).to_matrix(frame)
+                vec = (self.get_position(ti) -
+                       center.get_position(ti)).to_matrix(frame)
             pos[:, idx] = vec[:]
         return pos
 
 
 class CircularOrbit2D(Orbit):
+    '''2D circular orbit, built as toy model of 3D orbit. DEPRECATED'''
+
     def __int__(self, frame, center, radius = 0.0,
                 omega = 0.0, phase = 0.0):
         super().__init__(frame, center)
@@ -78,18 +90,21 @@ class CircularOrbit2D(Orbit):
 
 
 class CircularOrbit3D(Orbit):
-    def __init__(self, parentFrame, frame = None, center = None, radius: float = 0,
-                 omega: float = 0, inclination: float = 0, phase: float = 0,
-                 ascNode: float = 0, color = (0, 0, 0)):
+    '''Model of a ideal 3D orbit'''
+
+    def __init__(self, parentFrame, frame = None, center = None,
+                 radius: float = 0, omega: float = 0, inclination: float = 0,
+                 phase: float = 0, ascNode: float = 0, color = (0, 0, 0)):
         super().__init__(frame, center, color)
         self.frame = parentFrame.orientnew('frame', 'Axis', (inclination, frame.x))
+        # todo: orient new frame with 'Space'
         self.radius = radius
-        self.omega = omega
-        self.phase = phase
-        self.ascNode = ascNode
+        self.omega = omega  # todo: rename parm
+        self.phase = phase  # todo: rename parm
+        self.ascNode = ascNode  # todo: rename parm
 
     def get_position(self, epoch, frame = None):
-        # given an epoch, returns a vector for position
+        '''given an epoch, returns a sympy vector for position'''
         # todo pass other frame argument
         r = self.radius
         om = self.omega
@@ -106,6 +121,7 @@ class CircularOrbit3D(Orbit):
             return x + y + z + self.center.get_position(epoch).express(frame)
 
     def plot_orbit2D(self, center = None):
+        # DEPRECATED
         t = linspace(0, 5, 3000)
         pos_x = []
         pos_y = []
@@ -122,32 +138,32 @@ class CircularOrbit3D(Orbit):
 
 
 if __name__ == '__main__':
+    # testing the orbit and plotter classes
     N = ReferenceFrame('N')
-    # frame, center, radius, omega, inclination,
-    # phase = 0.0, ascNode = 0.0
-    orbit = CircularOrbit3D(N, N, None, 1, 1, 0, color = (1, 1, 1))
-    t = linspace(0, 0.5, 500)
-    v = orbit.get_trajectory(t, frame = N)
-    sat1_orbit = CircularOrbit3D(N, N, center = orbit, radius = 0.1,
+    t = linspace(0, 0.5, 500)  # numpy array
+
+    # creates 3 orbits. Two objects orbiting a main body
+    earth_orbit = CircularOrbit3D(N, N, None, 1, 1, 0, color = (1, 1, 1))
+    sat1_orbit = CircularOrbit3D(N, N, center = earth_orbit, radius = 0.1,
                                  omega = 100, inclination = np.pi / 2,
                                  color = (0, 1, 0))
-    sat2_orbit = CircularOrbit3D(N, N, center = orbit, radius = 0.08,
+    sat2_orbit = CircularOrbit3D(N, N, center = earth_orbit, radius = 0.08,
                                  omega = 120, inclination = np.pi / 6,
                                  color = (0, 0, 1))
 
-    Plotter.plot_trajectory(v, tube_radius = None, color = orbit.color)
-
-    Plotter.plot_trajectory(sat1_orbit.get_trajectory(t, frame = N),
-                            tube_radius = None, color = sat1_orbit.color)
-    Plotter.plot_trajectory(sat2_orbit.get_trajectory(t, frame = N),
-                            tube_radius = None, color = sat2_orbit.color)
-
-    Plotter.plot_point(sat1_orbit.get_position(0, N).to_matrix(N),
-                       scale_factor = 0.01, color = sat1_orbit.color)
-    Plotter.plot_point(sat2_orbit.get_position(0, N).to_matrix(N),
-                       scale_factor = 0.01, color = sat2_orbit.color)
+    # testing plotter functionalities
+    orbits = [earth_orbit, sat1_orbit, sat2_orbit]
+    for obt in orbits:
+        Plotter.plot_trajectory(obt.get_trajectory(t, frame = N),
+                                tube_radius = None, color = obt.color)
+        Plotter.plot_point(obt.get_position(0, N).to_matrix(N),
+                           scale_factor = 0.01, color = obt.color)
 
     Plotter.plot_separation(sat1_orbit.get_position(0, N).to_matrix(N),
                             sat2_orbit.get_position(0, N).to_matrix(N),
                             tube_radius = None, color = (1, 1, 1))
-    mlab.show()
+    Plotter.plot_vector(sat1_orbit.get_position(0, N).to_matrix(N),
+                        sat2_orbit.get_position(0, N).to_matrix(N) -
+                        sat1_orbit.get_position(0, N).to_matrix(N))
+
+    mlab.show()  # calls mayavi show
